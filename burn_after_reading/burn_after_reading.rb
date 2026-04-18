60c27a72e292e858682124fb2dffd1c965370a82
Jake Hash <1508098+jhash@users.noreply.github.com>
2026-04-18 18:03:41 -0400

    Try different fire chars

--- burn_after_reading/burn_after_reading.rb
+++ burn_after_reading/burn_after_reading.rb
@@ -1,13 +1,19 @@
 STDOUT.sync = true
 
+def move_to(x, y)
+  print "\e[#{y};#{x}H"
+end
+
 def print_char(x, y, char)
-  print "\e[#{y};#{x}H#{char}"
+  move_to(x, y)
+  print char
 end
 
 # █▓▒░
 FIRE_FADE_OUT_CHARS = ['█', '▓', '▒', '░'].freeze
 FIRE_FADE_IN_CHARS = FIRE_FADE_OUT_CHARS.dup.reverse.freeze
 FIRE_CHARS = (FIRE_FADE_IN_CHARS + FIRE_FADE_OUT_CHARS).freeze
+# FIRE_CHARS = ['.', ':', '-', '=', '+', '*', '#', '%', '@'].freeze
 
 def burn_char(x, y)
   Thread.new do
@@ -26,6 +32,7 @@ begin
   # Example output: gfmt1:cflag=4b00:iflag=6b02:lflag=200005cb:oflag=3:discard=f:dsusp=19:eof=4:eol=ff:eol2=ff:erase=7f:intr=3:kill=15:lnext=16:min=1:quit=1c:reprint=12:start=11:status=14:stop=13:susp=1a:time=0:werase=17:ispeed=9600:ospeed=9600
   old_state = `stty -g`
 
+  # Allow raw input
   system("stty raw -echo")
 
   # enter alt screen
